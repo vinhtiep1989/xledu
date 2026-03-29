e8d8417a4ce41961689316d3164537181ab6a7bc
vinhtiep1989 <vinhtiep1989@gmail.com>
2026-03-29 11:32:37 +0700

    update

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,11 @@
+@echo off
+echo 1. Dang luu code len Git...
 git add .
 git commit -m "update"
 git push
+echo.
+echo 2. Dang cap nhat web len Firebase...
+firebase deploy --project xledu-dea6a
+echo.
+echo XONG! DA LUU GIT VA CAP NHAT WEB TREN MANG THANH CONG!
+pause
